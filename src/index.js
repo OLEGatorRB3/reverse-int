b18cecf12b2954d4c3f01286df8ec5cf3d334584
OLEGatorRB3 <olegatorrb3@gmail.com>
2023-02-24 15:46:29 +0300

    doc: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+
+
 module.exports = function reverse (n) {
-  
+    if(n < 0) n = Math.abs(n)
+     let str = n.toString();
+    let reverseArr = str.split('').reverse()
+    let reverseStr = reverseArr.join('')
+    let num = Number(reverseStr)
+    return num 
 }
